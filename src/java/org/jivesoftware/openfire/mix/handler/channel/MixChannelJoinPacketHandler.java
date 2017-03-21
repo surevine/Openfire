5e66e131961243fc408a2da21acbda25971dbcc8
Philip Roberts <phil.roberts@surevine.com>
2017-03-21 09:53:22 +0000

    Return proxy jid on join

--- src/java/org/jivesoftware/openfire/mix/handler/channel/MixChannelJoinPacketHandler.java
+++ src/java/org/jivesoftware/openfire/mix/handler/channel/MixChannelJoinPacketHandler.java
@@ -9,15 +9,18 @@ import org.dom4j.Node;
 import org.dom4j.QName;
 import org.jivesoftware.openfire.mix.MixManager;
 import org.jivesoftware.openfire.mix.MixPersistenceManager;
+import org.jivesoftware.openfire.mix.constants.ChannelJidVisibilityPreference;
 import org.jivesoftware.openfire.mix.exception.CannotJoinMixChannelException;
 import org.jivesoftware.openfire.mix.handler.MixRequestContext;
 import org.jivesoftware.openfire.mix.model.MixChannel;
 import org.jivesoftware.openfire.mix.model.MixChannelParticipant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xmpp.forms.FormField;
 import org.xmpp.packet.IQ;
 import org.xmpp.packet.Message;
 import org.xmpp.packet.Presence;
+import org.xmpp.forms.DataForm;
 
 public class MixChannelJoinPacketHandler implements MixChannelPacketHandler {
 
@@ -44,6 +47,33 @@ public class MixChannelJoinPacketHandler implements MixChannelPacketHandler {
 			}
 		}
 
+		//TODO: is there a better way to parse data forms?
+		Element configForm = joinNode.element(QName.get("x", "jabber:x:data"));
+		ChannelJidVisibilityPreference jvp = ChannelJidVisibilityPreference.NO_PREFERENCE;
+
+		if (configForm != null) {
+			DataForm form = new DataForm(configForm);
+			FormField field = form.getField("JID Visibility");
+			String jidVisibilityPreference = field.getFirstValue();
+
+			switch (jidVisibilityPreference) {
+				case "no-preference":
+					jvp = ChannelJidVisibilityPreference.NO_PREFERENCE;
+					break;
+				case "prefer-hidden":
+					jvp = ChannelJidVisibilityPreference.PREFER_HIDDEN;
+					break;
+				case "enforce-hidden":
+					jvp = ChannelJidVisibilityPreference.ENFORCE_HIDDEN;
+					break;
+				case "enforce-visible":
+					jvp = ChannelJidVisibilityPreference.ENFORCE_VISIBLE;
+					break;
+				default:
+					jvp = ChannelJidVisibilityPreference.NO_PREFERENCE;
+			}
+		}
+
 		MixChannelParticipant mcp = null;
 
 		IQ result = IQ.createResultIQ(iq);
@@ -51,14 +81,14 @@ public class MixChannelJoinPacketHandler implements MixChannelPacketHandler {
 		Element joinElement = result.setChildElement("join", MixManager.MIX_NAMESPACE);
 
 		try {
-			mcp = channel.addParticipant(iq.getFrom(), subscriptionRequests);
+			mcp = channel.addParticipant(iq.getFrom(), subscriptionRequests, jvp);
 
 			for (String subscription : mcp.getSubscriptions()) {
 				Element current = joinElement.addElement("node");
 				current.addAttribute("node", subscription);
 			}
 
-			//joinElement.addAttribute("jid", mcp.getJid().toBareJID());
+			joinElement.addAttribute("jid", mcp.getJid().toBareJID());
 		} catch (CannotJoinMixChannelException e) {
 		    Log.error(e.toString());
 			result.setType(IQ.Type.error);

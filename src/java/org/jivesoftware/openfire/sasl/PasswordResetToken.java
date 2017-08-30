9ef8b4e42d1662192ac03b68ef34bfb61473aedf
Dave Cridland <dave@cridland.net>
2017-08-30 15:35:25 +0100

    SASL2: Add password reset mechanism
    This is driven by a user property containing the
    token in the format <expiry as UNIX time>,
    followed by a "|", followed by an alphanumeric
    random string. The user property name is
    "openfire.password-reset-token".
    
    To aid with testing, the expiry can be "0",
    meaning it never expires.
    
    The mechanism, PASSWORD-RESET-TOKEN, is a
    client-speaks-first mechanism. The client sends
    its requested username, followed by a NUL byte,
    followed by the alphanumeric random string
    stored on the server.
    
    If this token matches (and has yet to expire),
    the "openfire.password.reset" user property
    is set with a timestamp of when the reset token
    was sent, for debugging purposes.
    
    The PASSWORD-RESET task will then be required.

---
+++ src/java/org/jivesoftware/openfire/sasl/PasswordResetToken.java
@@ -0,0 +1,121 @@
+package org.jivesoftware.openfire.sasl;
+
+import org.jivesoftware.openfire.user.ResetToken;
+import org.jivesoftware.openfire.user.User;
+import org.jivesoftware.openfire.user.UserManager;
+import org.jivesoftware.openfire.user.UserNotFoundException;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.sasl.AuthorizeCallback;
+import javax.security.sasl.Sasl;
+import javax.security.sasl.SaslException;
+import javax.security.sasl.SaslServer;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class PasswordResetToken implements SaslServer {
+    private boolean completed = false;
+    private String username = null;
+    private CallbackHandler cbh = null;
+    public static final String MECH_NAME = "PASSWORD-RESET-TOKEN";
+    public static final String USERPROP_TOKEN = "openfire.password-reset-token";
+
+    public PasswordResetToken(CallbackHandler cbh) throws SaslException {
+        this.cbh = cbh;
+    }
+
+    @Override
+    public String getMechanismName() {
+        return MECH_NAME;
+    }
+
+    @Override
+    public byte[] evaluateResponse(byte[] bytes) throws SaslException {
+        /**
+         * Only response (may be initial) is username \0 access-token
+         */
+        if (bytes == null || bytes.length == 0) {
+            return null;
+        }
+        try {
+            completed = true;
+            StringTokenizer tokens = new StringTokenizer(new String(bytes, StandardCharsets.UTF_8), "\0");
+            username = tokens.nextToken();
+            String access_token = tokens.nextToken();
+            AuthorizeCallback acb = new AuthorizeCallback(username,username);
+            cbh.handle(new Callback[]{acb});
+            if(acb.isAuthorized()) {
+                username = acb.getAuthorizedID();
+                completed = true;
+            } else {
+                completed = true;
+                username = null;
+                throw new SaslException("PASSWORD-RESET-TOKEN: user not authorized: "+username);
+            }
+            User user = UserManager.getInstance().getUser(username);
+            Map<String, String> userProperties = user.getProperties();
+            if (userProperties.containsKey(USERPROP_TOKEN)) {
+                ResetToken stored = new ResetToken(userProperties.get(USERPROP_TOKEN));
+                ResetToken offered = new ResetToken(null, access_token);
+                if (offered.matches(stored)) {
+                    // User should now change their password.
+                    userProperties.put("openfire.password.reset", new Date().toString());
+                    userProperties.remove(USERPROP_TOKEN);
+                    return null;
+                }
+            }
+        } catch (UserNotFoundException e) {
+            throw new SaslFailureException(Failure.NOT_AUTHORIZED);
+        } catch (UnsupportedCallbackException e) {
+            throw new SaslFailureException(Failure.NOT_AUTHORIZED);
+        } catch (IOException e) {
+            throw new SaslFailureException(Failure.NOT_AUTHORIZED);
+        }
+        throw new SaslFailureException(Failure.NOT_AUTHORIZED);
+    }
+
+    @Override
+    public boolean isComplete() {
+        return completed;
+    }
+
+    @Override
+    public String getAuthorizationID() {
+        if (!completed) throw new IllegalStateException("Password reset not completed");
+        return username;
+    }
+
+    @Override
+    public byte[] unwrap(byte[] bytes, int i, int i1) throws SaslException {
+        throw new IllegalStateException("Unwrap for password reset");
+    }
+
+    @Override
+    public byte[] wrap(byte[] bytes, int i, int i1) throws SaslException {
+        throw new IllegalStateException("Unwrap for password reset");
+    }
+
+    @Override
+    public Object getNegotiatedProperty(String propName) {
+        if (completed) {
+            if (propName.equals(Sasl.QOP)) {
+                return "auth";
+            } else {
+                return null;
+            }
+        } else {
+            throw new IllegalStateException("PLAIN authentication not completed");
+        }
+    }
+
+    @Override
+    public void dispose() throws SaslException {
+        username = null;
+        completed = false;
+    }
+}

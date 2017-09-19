4f419327bc6fd0cc5355bcc31ff17a45baf0b714
Philip Roberts <phil.roberts@surevine.com>
2017-09-19 16:37:48 +0100

    SASL2 implementation

---
+++ src/java/org/jivesoftware/openfire/net/TotpSetupTask.java
@@ -0,0 +1,84 @@
+package org.jivesoftware.openfire.net;
+
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
+import com.warrenstrange.googleauth.GoogleAuthenticatorQRGenerator;
+import org.jivesoftware.openfire.XMPPServer;
+import org.jivesoftware.openfire.auth.AuthFactory;
+import org.jivesoftware.openfire.sasl.Failure;
+import org.jivesoftware.openfire.sasl.SaslFailureException;
+import org.jivesoftware.openfire.user.User;
+import org.jivesoftware.util.JiveGlobals;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Not client sends first (ie, no initial response)
+ * Server "challenge" is TOTP URI.
+ * Client sends TOTP code to complete.
+ * SASL2 framework will then demand a TOTP code, which is unfortunate.
+ */
+
+public class TotpSetupTask implements PostAuthenticationTask {
+    private static final Logger Log = LoggerFactory.getLogger(TotpSetupTask.class);
+
+    private boolean completed = false;
+    private User user;
+    private static GoogleAuthenticator googleAuthenticator = null;
+    private GoogleAuthenticatorKey key = null;
+    TotpSetupTask(User user) throws SaslFailureException {
+        this.user = user;
+        if (googleAuthenticator == null) {
+            googleAuthenticator = new GoogleAuthenticator();
+        }
+        if (!JiveGlobals.getBooleanProperty("openfire.totp", false)) {
+            throw new SaslFailureException(Failure.INVALID_MECHANISM, "TOTP not enabled here.");
+        }
+    }
+
+
+    @Override
+    public byte[] evaluateResponse(byte[] response) throws SaslFailureException {
+        if (response != null && response.length > 0) {
+            // Expecting 6 ASCII digits.
+            if (response.length != 6) {
+                throw new SaslFailureException(Failure.MALFORMED_REQUEST, "TOTP code too short");
+            }
+            if (key == null) {
+                throw new SaslFailureException(Failure.MALFORMED_REQUEST, "No TOTP code being setup");
+            }
+            int totpCode = 0;
+            for (byte responseByte : response) {
+                if (responseByte >= '0' && responseByte <= '9') {
+                    totpCode *= 10;
+                    totpCode += (responseByte - '0');
+                } else {
+                    throw new SaslFailureException(Failure.INCORRECT_ENCODING, "TOTP code expected as ASCII");
+                }
+            }
+            if (googleAuthenticator == null) {
+                googleAuthenticator = new GoogleAuthenticator();
+            }
+
+            Log.debug("Authorizing secret on setup: {}, code: {}", key.getKey(), totpCode);
+            boolean OK = googleAuthenticator.authorize(key.getKey(), totpCode);
+            completed = true;
+            if (!OK) {
+                throw new SaslFailureException(Failure.NOT_AUTHORIZED, "TOTP code error");
+            }
+            user.getProperties().put("openfire.totp.secret", AuthFactory.encryptPassword(key.getKey()));
+            return null;
+        }
+        key = googleAuthenticator.createCredentials();
+
+        String userId = user.getUsername() + "@" + XMPPServer.getInstance().getServerInfo().getXMPPDomain();
+        String uri = "otpauth://totp/" + "Openfire:" + userId + "?secret=" + key.getKey() + "&issuer=Openfire";
+        // String uri = GoogleAuthenticatorQRGenerator.getOtpAuthTotpURL("Openfire", user.getUsername() + "@" + XMPPServer.getInstance().getServerInfo().getXMPPDomain(), key);
+        return uri.getBytes();
+    }
+
+    @Override
+    public boolean isCompleted() {
+        return completed;
+    }
+}
---
+++ src/java/org/jivesoftware/openfire/user/ResetToken.java
@@ -0,0 +1,49 @@
+package org.jivesoftware.openfire.user;
+
+import org.jivesoftware.util.StringUtils;
+
+import java.util.Date;
+import java.util.StringTokenizer;
+
+public class ResetToken {
+    private final String token;
+    private final Date expiry;
+
+    public ResetToken(Date expiry, String token) {
+        if (expiry == null) {
+            this.expiry = new Date();
+        } else {
+            this.expiry = expiry;
+        }
+        this.token = token;
+    }
+    public ResetToken() {
+        this.expiry = new Date();
+        this.token = StringUtils.randomString(20);
+    }
+    public ResetToken(Long ttl) {
+        this.expiry = new Date(new Date().getTime() + ttl);
+        this.token = StringUtils.randomString(20);
+    }
+    public ResetToken(String encoded) {
+        StringTokenizer toks = new StringTokenizer(encoded, "|");
+        long expiry = new Long(toks.nextToken());
+        this.expiry = new Date(expiry);
+        this.token = toks.nextToken();
+    }
+
+    public String getToken() { return this.token; }
+
+    public boolean matches(ResetToken other) {
+        if (this.expiry.before(other.expiry) || this.expiry.equals(other.expiry) || other.expiry.equals(new Date(0L))) {
+            if (this.token.equals(other.token)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String toString() {
+        return "" + this.expiry.getTime() + "|" + this.token;
+    }
+}

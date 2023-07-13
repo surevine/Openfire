0055dd63a567181af114922c1b4ce2f5fefe0941
Matthew Vivian <matthew.vivian@surevine.com>
2023-07-13 16:01:22 +0100

    WIP: OF-2559 DirectTLS working but needs testing
    Need to implement DNS SRV records in the docker federated test rig to support Dialback over S2S.

--- xmppserver/src/main/java/org/jivesoftware/openfire/nio/NettySessionInitializer.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/nio/NettySessionInitializer.java
@@ -19,9 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.net.ssl.SSLException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
